3a9f0b7343557ea24f4664c490f1c192519b8d17
Mary Andrea Doria Gomez <monadord159@gmail.com>
2023-03-29 15:39:58 -0500

    Agregar los archivos fuente

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentosPlatzi/FundamentosPlatziApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamentosPlatzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosPlatziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosPlatziApplication.class, args);
+	}
+
+}
