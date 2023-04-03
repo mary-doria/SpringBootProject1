fcb12284e008c3dfc25c32a379a1969d8be02611
Mary Andrea Doria Gomez <monadord159@gmail.com>
2023-04-03 08:46:31 -0500

    DataSource a nivel de Clase

--- src/main/java/com/fundamentosplatzi/springboot/fundamentosPlatzi/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentosPlatzi/configuration/GeneralConfiguration.java
@@ -5,8 +5,12 @@ import com.fundamentosplatzi.springboot.fundamentosPlatzi.bean.MyBeanWithPropert
 import com.fundamentosplatzi.springboot.fundamentosPlatzi.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
 @EnableConfigurationProperties(UserPojo.class) // La clase se va a representar como propiedades dentro de nuestra aplicacion
 @Configuration
 public class GeneralConfiguration {
@@ -22,5 +26,17 @@ public class GeneralConfiguration {
     @Bean//Llamamos un Bean para utilizarlas
     public MyBeanWithProperties function(){
         return new MyBeanWithPropertiesImplement(name , lastname);
+    }
+
+    //Configuracion Manual a nivel de base de Datos
+    @Bean
+    public DataSource dataSource(){
+        //Se relaciona toda la configuracion con la base de datos
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem_testdb");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
     }
 }
